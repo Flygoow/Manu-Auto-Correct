585240eeefbf455649211bc98c3bdfc04d28e45c
Jérémy <30751347+Flygoow@users.noreply.github.com>
2018-10-24 23:36:30 +0200

    Update correct.js

--- js/correct.js
+++ js/correct.js
@@ -6,13 +6,13 @@ const pExp = new RegExp('MERCI', 'gi');
 const fExp = new RegExp('👍', 'gi');
 
 while(textNode=walk.nextNode()) {
-  textNode.nodeValue = textNode.nodeValue.replace(rExp, '🤙 É mercé hein 🤙');
-  textNode.nodeValue = textNode.nodeValue.replace(mExp, '🤙 é mercé hein 🤙');
-  textNode.nodeValue = textNode.nodeValue.replace(pExp, '🤙 É MERCÉ HEIN 🤙');
+  textNode.nodeValue = textNode.nodeValue.replace(rExp, 'É mercé 🤙');
+  textNode.nodeValue = textNode.nodeValue.replace(mExp, 'é mercé 🤙');
+  textNode.nodeValue = textNode.nodeValue.replace(pExp, 'É MERCÉ 🤙');
   textNode.nodeValue = textNode.nodeValue.replace(fExp, '👍');
 }
 
-document.title = document.title.replace(rExp, '🤙 É mercé hein 🤙');
-document.title = document.title.replace(mExp, '🤙 é mercé hein 🤙');
-document.title = document.title.replace(pExp, '🤙 É MERCÉ HEIN 🤙');
+document.title = document.title.replace(rExp, 'É mercé 🤙');
+document.title = document.title.replace(mExp, 'é mercé 🤙');
+document.title = document.title.replace(pExp, 'É MERCÉ 🤙');
 document.title = document.title.replace(fExp, '👍');
