0bfbabbc2f5f2b94bd84b21f52af6045811a8c1a
Jérémy <30751347+Flygoow@users.noreply.github.com>
2018-10-24 23:12:22 +0200

    Update correct.js

--- js/correct.js
+++ js/correct.js
@@ -3,22 +3,12 @@ const walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT,null,
 const rExp = new RegExp('Merci|'+
                         'Mercy|'+
                         'Mrc|');
-const zExp = new RegExp('merci|'+
-                        'mercy|'+
-                        'mrc|');
-const eExp = new RegExp('MERCI|'
-                        +'MERCY|'
-                        +'MRC|');
 const pExp = new RegExp('👍');
 
 while(textNode=walk.nextNode()) {
     textNode.nodeValue = textNode.nodeValue.replace(rExp, 'É mercé');
-    textNode.nodeValue = textNode.nodeValue.replace(zExp, 'é mercé');
-    textNode.nodeValue = textNode.nodeValue.replace(eExp, 'É MERCÉ');
     textNode.nodeValue = textNode.nodeValue.replace(pExp, '🤙');
 }
 
 document.title = document.title.replace(rExp, 'É mercé');
-document.title = document.title.replace(zExp, 'é mercé');
-document.title = document.title.replace(eExp, 'É MERCÉ');
 document.title = document.title.replace(pExp, '🤙');
